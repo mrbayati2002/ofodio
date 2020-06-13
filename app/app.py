80da3edd95f250eca7d4f794e13b4bfa85523e69
Mohammad Reza Bayati <mrbayati2002@gmail.com>
2020-06-13 15:43:47 +0430

    implement wtforms for add movie, but not work

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ from flask import (
     flash,
     redirect,
     request,
-
 )
+from forms import AddMovieForm
 import config
 from werkzeug.utils import secure_filename
 import os
@@ -20,76 +20,49 @@ app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 @app.route('/')
 @app.route('/home')
 def home():
-    active = {'home':'active', 'search':'', 'full_list':'', 'newmovie':'', 'about':'',}
+    active = {'home':'active', 'search':'', 'full_list':'', 'addmovie':'', 'about':'',}
     return render_template('home.html', data= {'active': active,
                                                     'copyrightmessage': config.COPYRIGHT_MESSAGE,
-                                                    'title':'Ofodio(Video platform) | Home',
-                                                    })
+                                                    'title':'Ofodio(Video platform) | Home', })
 
 @app.route('/search')
 def search():
-    active = {'home':'active', 'search':'active', 'full_list':'', 'newmovie':'', 'about':'',}
+    active = {'home':'active', 'search':'active', 'full_list':'', 'addmovie':'', 'about':'',}
     return render_template('search.html', data= {'active': active,
                                                     'copyrightmessage': config.COPYRIGHT_MESSAGE,
-                                                    'title':'Ofodio | Search',
-                                                    })
+                                                    'title':'Ofodio | Search', })
 
 @app.route('/full_list')
 def full_list():
-    active = {'home':'active', 'search':'', 'full_list':'active', 'newmovie':'', 'about':'',}
+    active = {'home':'active', 'search':'', 'full_list':'active', 'addmovie':'', 'about':'',}
     return render_template('full_list.html', data= {'active': active,
                                                     'copyrightmessage': config.COPYRIGHT_MESSAGE,
-                                                    'title':'Ofodio | Movies complete list',
-                                                    })
+                                                    'title':'Ofodio | Movies complete list', })
 
 @app.route('/about')
 def about():
-    active = {'home':'', 'search':'', 'full_list':'', 'newmovie':'', 'about':'active',}
+    active = {'home':'', 'search':'', 'full_list':'', 'addmovie':'', 'about':'active',}
     return render_template('/about.html', data= {'active': active,
                                                     'copyrightmessage': config.COPYRIGHT_MESSAGE,
-                                                    'title':'About',
-                                                    })
+                                                    'title':'About', })
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/newmovie', methods=['POST', 'GET'])
-def newmovie():
-    form_items = config.FORM_ITEMS_TO_EMPTY_CHECK
-    # print(request.form['m_cover'])
-    if request.method == 'POST':
-        # check if the post request has the file part
-        # for item in form_items.items():
-        #     if request.form[item[0]] == '':
-        #         flash('Please fill out %s field,  just Summary and Rank are optional.'%item[1], 'danger')
-        #         return redirect(request.url)
-        if 'm_file' not in request.files:
-            flash('No file part', 'danger')
-            return redirect(request.url)
-        file = request.files['m_file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file', 'danger')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('uploaded_file', filename=filename))
-            # return render_template('seccessful.html', movie_name = filename)
-            flash('Seccussfully created :)', 'success')
-            return redirect('/newmovie')
-    active = {'home':'', 'search':'', 'full_list':'', 'newmovie':'active', 'about':'',}
-    return render_template('newmovie.html', data={'active':active,
+@app.route('/addmovie', methods=['POST', 'GET'])
+def addmovie():
+    form = AddMovieForm
+    active = {'home':'', 'search':'', 'full_list':'', 'addmovie':'active', 'about':'',}
+    return render_template('addmovie.html', form=form, data={'active':active,
                                                 'copyrightmessage':config.COPYRIGHT_MESSAGE,})
 
-def database_connection():
-    import sqlite3
-    con = sqlite3.conenct('%s'%config.DB_NAME)
-    c = con.cursor()
+# def database_connection():
+#     import sqlite3
+#     con = sqlite3.conenct('%s'%config.DB_NAME)
+#     c = con.cursor()
 
 if __name__ == '__main__':
-    app.run(debug = True, port=5005, host='0.0.0.0')
-    # app.run(debug = True, port=5005)
+    # app.run(debug = True, port=5005, host='0.0.0.0')
+    app.run(debug = True, port=5005)
 
---
+++ app/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField,
+                        SubmitField,
+                        DateField,
+                        IntegerField,
+                        FloatField,
+                        TextAreaField,
+                        FileField,
+                        SelectField,
+                        BooleanField,
+                        SubmitField,)
+from wtforms.validators import DataRequired, Length, regexp
+
+GENRES = (
+    ('none', 'بدون دسته بندی'),
+    ('action', 'اکشن'),
+    ('historical', 'تاریخی'),
+    ('horror', 'ترسناک'),
+    ('crime', 'جنایی'),
+    ('war', 'جنگی'),
+    ('family', 'خانوادگی'),
+    ('news', 'خبری'),
+    ('drama', 'درام'),
+    ('mysterious', 'راز آلود'),
+    ('biography', 'زندگی نامه'),
+    ('science_fiction', 'علمی-تخیلی'),
+    ('fantasy', 'فانتزی'),
+    ('comedy', 'کمدی'),
+    ('short', 'کوتاه'),
+    ('adventure', 'ماجراجویی'),
+    ('documentary', 'مستند'),
+    ('musical', 'موزیکال'),
+    ('thriller', 'هیجان انگیز'),
+    ('sport', 'ورزشی'),
+    ('western', 'وسترن'),
+)
+
+
+class AddMovieForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=1, max=100)])
+    pubdate = DateField('pubdate', validators=[DataRequired()])
+    director = StringField('director', validators=[DataRequired(), Length(min=0, max=30)])
+    country = StringField('country', validators=[DataRequired(), Length(min=0, max=16)])
+    language = StringField('language', validators=[DataRequired(), Length(min=2, max=16)])
+    length = IntegerField('length', validators=[DataRequired(), Length(min=0, max=3)])
+    category = SelectField('category', choices=GENRES, validators=[DataRequired()])
+    rank = FloatField('Rank', validators=[Length(min=0, max=10)])
+    summary = TextAreaField('Summary', validators=[Length(max=1000)])
+    cover = FileField('Cover', validators=[regexp(r'^[^/\\]\.jpg$')])
+    moviefile = FileField('Movie File', validators=[DataRequired()])
+    have_subtitle = BooleanField('Subtitle ?', validators=[DataRequired()])
+    subtitle = FileField('Subtitle')
+    submit = SubmitField('Add')
